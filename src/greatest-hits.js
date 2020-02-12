a55a3964f8bc483bd81256191fd077894606a28a
Brandan Sayarath and Keturah Howard <keturah@>
2020-02-12 15:18:01 -0800

    add try catch statements

--- src/greatest-hits.js
+++ src/greatest-hits.js
@@ -6,21 +6,27 @@ export class Songs {
   }
 
   async getFetch(input) {
-    let songs = await fetch(`https://genius.p.rapidapi.com/search?q=${input}`, {
-      'method': 'GET',
-      'headers': {
-        'x-rapidapi-host':  "genius.p.rapidapi.com",
-        "x-rapidapi-key": `${process.env.API_KEY}`
-      }
+    try {
+      let songs = await fetch(`https://genius.p.rapidapi.com/sear?q=${input}`, {
+        'method': 'GET',
+        'headers': {
+          'x-rapidapi-host':  "genius.p.rapidapi.com",
+          "x-rapidapi-key": `${process.env.API_KEY}`
+        }
 
-    });
-    console.log(songs.status)
-    if (songs.status != 200) {
-      this.status = false
-    } else {
-      let json = await songs.json();
-      this.jsonifiedObj = json.response;
+      });
+      console.log(songs.status)
+      if (songs.status != 200) {
+        this.status = false
+      } else {
+        let json = await songs.json();
+        this.jsonifiedObj = json.response;
+      }
+    } catch(error) {
+        console.warn('oh no whatcha do??');
     }
-  }
+  } 
+ 
+
 
 }
